62fdbb6898e4d0e92098497d186222d7dce62439
meganmaida <90005500+meganmaida@users.noreply.github.com>
2021-12-13 11:41:01 -0700

    Create selfportrait.js

---
+++ Homework 9/selfportrait.js
@@ -0,0 +1,53 @@
+function setup ()
+    {
+        createCanvas (600, 600);
+    }
+ function draw ()
+    {
+     background (200, 245, 330);
+        fill ("green");
+    rect (0, 470, 600, 600);
+    triangle (320, 30, 192, 200, 400, 200)
+        fill ("yellow")
+    circle (320, 32, 60);
+        fill ("pink");
+    ellipse (300, 350, 280, 320);
+    ellipse (470, 330, 110, 90);
+        fill ("red")
+    ellipse (420, 500, 110, 95);
+    ellipse (185, 500, 110, 95);
+        rotate(radians(-35))
+        fill ("pink");
+    ellipse (-50, 310, 90, 110);
+        fill ("yellow");
+        rotate(radians(39))
+    rect (205, 170, 230, 60)
+        fill ("black")
+    ellipse (360, 305, 30, 60)
+    ellipse (295, 303, 30, 60)
+        fill ("white")
+    ellipse (360, 295, 27, 33)
+    ellipse (295, 293, 27, 33)
+        fill ("black")
+        rotate(radians(35))
+    rect (448, 7, 7, 50)
+        rotate(radians(-75))
+    rect (46, 388, 7, 50)
+        fill ("maroon")
+        rotate(radians(35))
+    ellipse (290, 406, 60, 40)
+        fill ("#fae")
+    ellipse (290, 415, 40, 20)
+    ellipse (220, 370, 40, 20)
+    ellipse (365, 385, 40, 20)
+    rotate (radians(1))
+        fill ("gray")
+    quad (110, 280, 110, 100, 160, 40, 135, 270)
+    line (118, 280, 140, 85)
+    line (120, 100, 150, 100)
+        fill ("blue")
+    quad (100, 300, 70, 220, 120, 260, 190, 250)
+        fill ("black")
+    text('This is Kirby. Kirby is my spirit animal, therefore, he is my representation of a self portrait', 10, 10, 70);
+    text('Megan Maida', 520, 590)
+    }
